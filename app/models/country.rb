c4685141afbd54c6184ea762329ccac9acd2af0d
Robert Forbes <robert@roboticdinosaur.co.uk>
2024-10-16 20:13:57 +0100

    Fix api call

--- app/models/country.rb
+++ app/models/country.rb
@@ -7,38 +7,18 @@ class Country < ApplicationRecord
 	has_many :leagues
 
 	def self.fetch_and_update_from_api
-		response = HTTParty.get('https://v3.football.api-sports.io/countries', headers: {
-			'x-rapidapi-host' => ENV['API_SPORTS_URI'],
-			'x-rapidapi-key'  => ENV['API_SPORTS_KEY']
-			#'x-apisports-key' => ENV['API_SPORTS_KEY']
-		})
+		api_client = ApiClient.new
+		remote_countries = api_client.fetch('countries')
 
-		puts response
-
-		if response.success?
-			remote_countries = response.parsed_response['response']
-
-			remote_countries.each do |remote_country|
-				country = Country.find_or_initialize_by(name: remote_country['name'])
-
-				if country.new_record? || country.country_updated?(remote_country)
-					country.update!(
-						name: remote_country['name'],
-						code: remote_country['code'],
-						flag: remote_country['flag'],
-						slug: normalized_name.parameterize
-					)
-
-					#country.generate_slug
-					country.save!
-				end
-			end
+		remote_countries.each do |remote_country|
+			create_or_update(remote_country)
 		end
 	end
 
 	def self.create_or_update(country_data)
 		normalized_name = normalize_name(country_data['name'])
-		country = Country.find_or_initialize_by(slug: normalized_name.parameterize)
+		slug = normalized_name.parameterize
+		country = Country.find_or_initialize_by(slug: slug)
 
 		if country.new_record?
 			puts "Create country"
@@ -46,7 +26,7 @@ class Country < ApplicationRecord
 				name: country_data['name'],
 				code: country_data['code'],
 				flag: country_data['flag'],
-				slug: normalized_name.parameterize
+				slug: slug
 			)
 		else
 			puts "Update country"
