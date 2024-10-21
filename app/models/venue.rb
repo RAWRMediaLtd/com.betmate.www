1bc4a7c196c9b313972c0c44165d5d2104bf61c0
Robert Forbes <robert@roboticdinosaur.co.uk>
2024-10-21 23:03:29 +0100

    Add last_synced_at to venue model.

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -3,30 +3,37 @@ class Venue < ApplicationRecord
 
 	has_many :fixtures
 
-	def self.fetch_and_update_from_api(id = nil)
+	def self.fetch_and_update_from_api(params = {})
 		#puts "Fetch and update from api - Venues"
 
-		url = "https://v3.football.api-sports.io/venues?id=#{id}"
+		#url = "https://v3.football.api-sports.io/venues?id=#{id}"
 
-		response = HTTParty.get(url, headers: {
-			'x-rapidapi-host' => ENV['API_SPORTS_URI'],
-			'x-rapidapi-key'  => ENV['API_SPORTS_KEY']
+		#response = HTTParty.get(url, headers: {
+		#	'x-rapidapi-host' => ENV['API_SPORTS_URI'],
+		#	'x-rapidapi-key'  => ENV['API_SPORTS_KEY']
 			#'x-apisports-key' => ENV['API_SPORTS_KEY']
-		})
+		#})
 
-		puts response
+#		puts response
 
-		if response.success?
-			remote_venues = response.parsed_response['response']
+#		if response.success?
+#			remote_venues = response.parsed_response['response']
+#			remote_venues.each do |remote_venue|
+#				Venue.find_or_initialize_and_update(remote_venue)
+#			end
+#		end
+
+		api_client = ApiClient.new
+
+		remote_venues = api_client.fetch('venues', params)
+		if remote_venues.present?
 			remote_venues.each do |remote_venue|
-				Venue.find_or_initialize_and_update(remote_venue)
+				Venue.create_or_update(remote_venue)
 			end
 		end
 	end
 
-	def self.find_or_create(venue_data)
-	  retries = 0
-
+	def self.create_or_update(venue_data)
     venue = Venue.find_or_initialize_by(id: venue_data['id'])
 
 		if venue.new_record?
@@ -41,7 +48,8 @@ class Venue < ApplicationRecord
 				capacity: venue_data['capacity'],
 				surface: venue_data['surface'],
 				image: venue_data['image'],
-				slug: name.to_slug.normalize.to_s
+				slug: name.to_slug.normalize.to_s,
+				last_synced_at: Time.now
 			)
 			# venue.slug ||= venue.name.parameterize
 		else
@@ -53,7 +61,8 @@ class Venue < ApplicationRecord
 				city: venue_data['city'],
 				capacity: venue_data['capacity'],
 				surface: venue_data['surface'],
-				image: venue_data['image']
+				image: venue_data['image'],
+				last_synced_at: Time.now
 			)
 		end
 
